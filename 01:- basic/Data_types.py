ec5bba0ed7a1061615d5999b35d67df8e98bac10
Jayshri Borase <jayshriborase25aiml04@gmail.com>
2026-08-20 17:12:15 +0530

    Add Python script for basic data types demonstration

---
+++ 01:- basic/Data_types.py
@@ -0,0 +1,17 @@
+# Python Data Types
+
+name = "Jayshri"
+age = 18
+cgpa = 8.0
+is_student = True
+
+print("Name:", name)
+print("Age:", age)
+print("CGPA:", cgpa)
+print("Student:", is_student)
+
+print("\nData Types:")
+print(type(name))
+print(type(age))
+print(type(cgpa))
+print(type(is_student))
