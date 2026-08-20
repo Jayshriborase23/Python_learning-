338711dc03a53aadac435816b10578fd8441e644
Jayshri Borase <jayshriborase25aiml04@gmail.com>
2026-08-20 16:58:26 +0530

    Create variable.py

---
+++ variable.py
@@ -0,0 +1,10 @@
+# Python Variables
+name = "Jayshri"
+age = 18
+course = "B.Tech AI/ML"
+university = "DBATU University"
+
+print("Name:", name)
+print("Age:", age)
+print("Course:", course)
+print("University:", university)
