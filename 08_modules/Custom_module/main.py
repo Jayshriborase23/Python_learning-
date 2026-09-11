9cad6710991b53f8f60b93b19e81619bb054fc94
Jayshri Borase <jayshriborase25aiml04@gmail.com>
2026-09-11 12:00:19 +0530

    Add main.py to demonstrate calculator module usage
    Implement main script to utilize calculator module functions.

---
+++ 08_modules/Custom_module/main.py
@@ -0,0 +1,11 @@
+import calculator
+
+print("===== Custom Module =====")
+
+a = 20
+b = 5
+
+print("Addition:", calculator.add(a, b))
+print("Subtraction:", calculator.subtract(a, b))
+print("Multiplication:", calculator.multiply(a, b))
+print("Division:", calculator.divide(a, b))
