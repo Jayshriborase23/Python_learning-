5464f3bafc2c21e6966e64544876682217710505
Jayshri Borase <jayshriborase25aiml04@gmail.com>
2026-09-11 07:52:51 +0530

    Add math module with various mathematical operations

---
+++ 08_modules/math_module.py
@@ -0,0 +1,13 @@
+import math
+
+print("===== Math Module =====")
+
+number = 25
+
+print("Number:", number)
+print("Square Root:", math.sqrt(number))
+print("Power:", math.pow(2, 3))
+print("Factorial:", math.factorial(5))
+print("Ceiling:", math.ceil(4.3))
+print("Floor:", math.floor(4.8))
+print("Value of Pi:", math.pi)
