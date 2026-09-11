7ee90d6034db954f928ed8f60bbfec790ff366e1
Jayshri Borase <jayshriborase25aiml04@gmail.com>
2026-09-11 11:53:00 +0530

    Create module.py

---
+++ 08_modules/module.py
@@ -0,0 +1,11 @@
+# Python Modules
+
+# Importing built-in module
+import math
+
+number = 25
+
+print("Number:", number)
+print("Square Root:", math.sqrt(number))
+print("Power:", math.pow(2, 3))
+print("Value of Pi:", math.pi)
